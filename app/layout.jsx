98da2193bb6a06fc79ae7f94c8b7ec8deca70c4e
sornersbooms <oscardanielyepez777@gmail.com>
2025-12-06 23:13:28 -0500

    gtm y ga4 agregado y configurado cinco

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { Analytics } from "@vercel/analytics/next"
 import { GoogleTagManager } from '@next/third-parties/google'
 export const metadata = {
+  metadataBase: new URL('https://espiritualia-three.vercel.app'),
   title: 'Espiritualia',
   description: 'Un viaje visual a través de conceptos espirituales.',
   manifest: '/manifest.json',
